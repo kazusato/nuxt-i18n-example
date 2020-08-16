ee28f085f5dbc96d5bafe8f4edbe935031063b27
Kazuhiko Sato <super80@gmail.com>
2020-08-17 06:23:38 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link :to="switchLocalePath('ja')">日本語</nuxt-link>
+      <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
+    </div>
+    <div>
+      <h1>{{$t('message')}}</h1>
+      <div>
+        <img :src="$t('imgfile')">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
